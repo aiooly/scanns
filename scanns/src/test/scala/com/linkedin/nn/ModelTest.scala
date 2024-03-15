9a77dae29c541cc98122c47f9c1caa36a393bfd9
Yan Li <yan.li@rea-group.com>
2024-03-15 18:03:24 +0800

    Add test example

--- scanns/src/test/scala/com/linkedin/nn/ModelTest.scala
+++ scanns/src/test/scala/com/linkedin/nn/ModelTest.scala
@@ -4,11 +4,13 @@
  */
 package com.linkedin.nn
 
-import com.linkedin.nn.algorithm.CosineSignRandomProjectionNNS
+import com.linkedin.nn.algorithm.{CosineSignRandomProjectionNNS, JaccardMinHashNNS}
 import com.linkedin.nn.lsh.SignRandomProjectionHashFunction
 import com.linkedin.nn.test.SparkTestUtils
 import com.linkedin.nn.utils.TopNQueue
-import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.ml.feature.{Tokenizer, Word2Vec}
+import org.apache.spark.ml.linalg.{Vector, Vectors}
+import org.apache.spark.sql.functions.monotonically_increasing_id
 import org.testng.annotations.Test
 
 /* A simple end-to-end run of a nearest neighbor search using the driver */
@@ -40,5 +42,48 @@ class ModelTest extends SparkTestUtils {
     val nbrs = model.getSelfAllNearestNeighbors(items, 10)
 
     print("nbrs: ", nbrs.take(10).mkString("Array(", ", ", ")"))
+  }
+
+
+  @Test
+  def testMinHash(): Unit = sparkTest("modelTest") {
+    sc.getConf.registerKryoClasses(Array(classOf[TopNQueue], classOf[SignRandomProjectionHashFunction]))
+
+    var df = sparkSession.read.option("header", true).csv("src/test/resources/text01.csv")
+
+    val tokenizer = new Tokenizer().setInputCol("question_text").setOutputCol("words")
+    val tokenized = tokenizer.transform(df)
+
+    // Learn a mapping from words to Vectors.
+    val word2Vec = new Word2Vec()
+      .setInputCol("words")
+      .setOutputCol("vectors")
+      .setVectorSize(3)
+      .setMinCount(0)
+    val word2VecModel = word2Vec.fit(tokenized)
+    val result = word2VecModel.transform(tokenized)
+
+    val items = result.select("id", "vectors").rdd.map(row => (row.getString(0).toLong, row.getAs[Vector](1)))
+
+    val numFeatures = items.values.take(1)(0).size
+
+//    val model = new JaccardMinHashNNS()
+//      .setNumHashes(200)
+//      .setSignatureLength(10)
+//      .setBucketLimit(10)
+//      .setJoinParallelism(5)
+//      .createModel(numFeatures)
+
+    val model = new CosineSignRandomProjectionNNS()
+      .setNumHashes(200)
+      .setSignatureLength(10)
+      .setBucketLimit(10)
+      .setJoinParallelism(5)
+      .createModel(numFeatures)
+
+    val nbrs = model.getSelfAllNearestNeighbors(items, 10)
+
+    nbrs.collect().foreach(row => println(row))
+
   }
 }
